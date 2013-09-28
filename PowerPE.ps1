4df5db7b93505fb7ac016de8a1e9e0474ad07734
Matthew Ratzke <matthew.003@me.com>
2013-09-27 21:10:29 -0600

    Initial Script

---
+++ PowerPE.ps1
@@ -0,0 +1,26 @@
+﻿# Check and Setup System Requirements
+
+# Load Needed Components
+Import-Module ShowUI -MinimumVersion 1.5.1
+
+
+#Load Base UI
+RibbonWindow { 
+   Ribbon -Items {
+      RibbonTab -Header "Task A" { 
+         RibbonGroup -Header "PoshCode" { 
+            # The ImageSource needs to be a path to an image...
+            RibbonButton -LargeImageSource "http://huddledmasses.org/images/icons/PoshCode.png" -Label "PoshCode"
+         }
+      }
+      RibbonTab -Header "Task B" 
+   }
+} -Title "PowerPE Builder" -Show
+
+#Populate System with Defaults
+
+#Override Defaults
+
+#Load Extensions
+
+#Run
