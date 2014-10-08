cec7372a75bacf4e7f659a0bcc35ead44be833c2
Matthew Ratzke <matthew.003@icloud.com>
2014-10-08 15:52:29 -0600

    Update psm1

--- PwrBuilder.psm1
+++ PwrBuilder.psm1
@@ -11,25 +11,3 @@ Get-ChildItem ("$PSScriptRoot\Communications\") | Where `
 Get-ChildItem ("$PSScriptRoot\Common\") | Where `
     { $_.Name -notlike '__*' -and $_.Name -like '*.ps1'} | ForEach `
     { . $_.FullName }
-
-
-#Load Base UI
-RibbonWindow { 
-   Ribbon -Items {
-      RibbonTab -Header "Task A" { 
-         RibbonGroup -Header "PoshCode" { 
-            # The ImageSource needs to be a path to an image...
-            RibbonButton -LargeImageSource "http://huddledmasses.org/images/icons/PoshCode.png" -Label "PoshCode"
-         }
-      }
-      RibbonTab -Header "Task B" 
-   }
-} -Title "PowerPE Builder" -Show
-
-#Populate System with Defaults
-
-#Override Defaults
-
-#Load Extensions
-
-#Run
